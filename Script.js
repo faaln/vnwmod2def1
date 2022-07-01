9228e84fd56ee86490ed56c2fa652039fe161c4b
faaln <34640250+faaln@users.noreply.github.com>
2022-07-01 04:44:22 -0300

    First commit

---
+++ Script.js
@@ -0,0 +1,29 @@
+// Crie uma variável que receba um nome;
+
+let name = 'Amilton Neto'
+
+// Crie uma variável que receba um número;
+
+let number = 26
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+let res = 2022 - number
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+
+console.log(res + number)
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+
+console.log('estou mexendo no console.log')
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+
+let quartaFeira = false
+
+console.log(quartaFeira)
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+
+console.log(typeof quartaFeira)
